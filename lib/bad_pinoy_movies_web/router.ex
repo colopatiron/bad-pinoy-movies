b7c5d50941f778be0c0cf9c563392a7297c35833
Jem Pimentel <jem.pimentel@outlook.com>
2020-04-05 21:45:44 +0800

    Creates auth plug

--- lib/bad_pinoy_movies_web/router.ex
+++ lib/bad_pinoy_movies_web/router.ex
@@ -7,6 +7,7 @@ defmodule BadPinoyMoviesWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug BadPinoyMovies.Auth
   end
 
   pipeline :api do
